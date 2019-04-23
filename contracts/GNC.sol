7329f0a67bd6f266d6d210e92dbb2a804407b181
swsong <songaal@gmail.com>
2019-04-23 18:20:53 +0900

    ..

--- contracts/GNC.sol
+++ contracts/GNC.sol
@@ -1,15 +1,16 @@
 pragma solidity ^0.5.2;
 
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+
 
 contract GNC is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable {
 
     constructor(
-        string name,
-        string symbol,
+        string memory name,
+        string memory symbol,
         uint8 decimals
     )
     ERC20Burnable()
