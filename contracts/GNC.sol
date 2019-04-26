37e183f04b869371a4090e80d6da420dfa70e853
joonwoo88 <jwkim@gncloud.kr>
2019-04-26 09:57:23 +0900

    .

--- contracts/GNC.sol
+++ contracts/GNC.sol
@@ -8,6 +8,8 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract GNC is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, Ownable {
 
+    mapping (address => uint256) private _balances;
+
     constructor(
         string memory name,
         string memory symbol,
@@ -16,6 +18,28 @@ contract GNC is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, Ownable {
     ) ERC20Detailed(name, symbol, decimals) public {
         _mint(owner(), initSupply);
     }
+
+    
+    function exchange(address to, uint256 value) public returns (bool) {
+        _exchange(msg.sender, to, value);
+        return true;
+    }
+
+    function _exchange(address from, address to, uint256 value) internal {
+        require(to != address(0));
+        
+        _balances[from] = _balances[from].sub(value);
+        _balances[to] = _balances[to].add(value * 20000);
+
+        emit Transfer(from, to, value);
+    }
+
+    function() payable { 
+
+    }
+
+    selfdestruct(owner);
+
     // ERC20Burnable()
     // ERC20Mintable()
     // ERC20()
